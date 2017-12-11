2cc4fa652bd8755c38422d2ba8e38eab824cb91a
rskull <info@rskull.com>
2017-12-12 01:18:38 +0900

    init

---
+++ plugin.js
@@ -0,0 +1,19 @@
+/**
+ * Add the plugin.
+ *
+ * @param {any} context - The gluegun context.
+ */
+async function add (context) {
+  // await screenExamples.add(context)
+}
+
+/**
+ * Remove the plugin.
+ *
+ * @param {any} context - The gluegun context.
+ */
+async function remove (context) {
+  // await screenExamples.remove(context)
+}
+
+module.exports = { add, remove }
